ab126a1a02ef337571e253b43fd57200b7d7b071
刘美 <liumei@jkweixin.net>
2019-07-29 16:29:14 +0800

    组件修改

---
+++ src/components/_base-table-tool/_base-table-tool.unit.js
@@ -0,0 +1,7 @@
+import BaseTableTool from './_base-table-tool'
+
+describe('@components/-base-table-tool', () => {
+  it('exports a valid component', () => {
+    expect(BaseTableTool).toBeAComponent()
+  })
+})
---
+++ src/components/_base-table-tool/_base-table-tool.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="base-table-tool">
+    <div class="title-wrap">
+      <img v-if="iconUrl" :src="iconUrl" class="title-icon">
+      <p v-if="title" class="title-name">{{title}}</p>
+    </div>
+    <slot name="center"></slot>
+    <div class="function-btn">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  const COMPONENT_NAME = 'BASE_TABLE_TOOL'
+
+  export default {
+    name: COMPONENT_NAME,
+    props: {
+      iconUrl: {
+        default: '',
+        type: String
+      },
+      title: {
+        default: '列表',
+        type: String
+      }
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .base-table-tool
+    width: 100%
+    background: $color-white
+    height: 72px
+    display: flex
+    align-items: center
+    justify-content: space-between
+    box-sizing: border-box
+    padding:0 20px
+    .title-wrap
+      display: flex
+      align-items: center
+      .title-icon
+        width: 14px
+        height: 14px
+        margin-right: 6px
+      .title-name
+        line-height: 1
+        white-space: nowrap
+        font-size: $font-size-16
+        font-family: $font-family-regular
+        color: $color-text-main
+
+    .function-btn
+      display: flex
+</style>
