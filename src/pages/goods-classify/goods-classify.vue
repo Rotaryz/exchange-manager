a0e285c7a05b557cc7ce693a524875098e540a68
刘美 <liumei@jkweixin.net>
2019-07-30 19:10:24 +0800

    增加路由

---
+++ src/pages/goods-classify/goods-classify.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="goods-classify">
+    goods-classify
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // import * as Helpers from './modules/helpers'
+  // import API from '@api'
+  const PAGE_NAME = 'GOODS_CLASSIFY'
+  const TITLE = '商品分类'
+
+  export default {
+    name: PAGE_NAME,
+    page: {
+      title: TITLE
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .goods-classify
+    width: 100%
+</style>
---
+++ src/pages/goods-classify/modules/helpers.js
@@ -0,0 +1,5 @@
+// import {mapState, mapGetters, mapActions, mapMutations} from 'vuex'
+//
+// export const goodsClassifyComputed = {
+//   ...mapGetters('goodsClassify', [])
+// }
