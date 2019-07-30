3537a746e1c7d6cea353636148648f67f3c4e666
zhouxuan <zhouxuan@jkweixin.com>
2019-07-30 18:43:29 +0800

    客户管理交互

---
+++ src/pages/order-list/order-list.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="order-list">
+    order-list
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  // import * as Helpers from './modules/helpers'
+  // import API from '@api'
+  const PAGE_NAME = 'ORDER_LIST'
+  const TITLE = '订单列表'
+
+  export default {
+    name: PAGE_NAME,
+    page: {
+      title: TITLE
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~@design"
+
+  .order-list
+    width: 100%
+</style>
