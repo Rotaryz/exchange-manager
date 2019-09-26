6676ee34ab47b8aa790738cf0859b0c32922172d
liujiajie <liujiajie@jkweixin.net>
2019-09-26 10:39:49 +0800

    新营销-页面管理修改

--- src/pages/course-page/course-page.vue
+++ src/pages/course-page/course-page.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="course-page">
-    <course-content :bannerList="bannerList" :cmsType.sync="type" :cmsList="moduleList"></course-content>
+    <course-content :bannerList="moduleObj[type].children" :cmsType.sync="type" :cmsList="moduleList"></course-content>
     <div class="edit-modular">
       <div class="box">
         <div class="small">
@@ -9,8 +9,8 @@
               <div class="content-title">轮播图设置</div>
               <div class="content-sub">(最多添加5个banner，鼠标拖拽调整广告顺序)</div>
             </div>
-            <slick-list v-model="bannerList" :distance="30" lockAxis="y">
-              <slick-item v-for="(item, index) in bannerList" :key="index" :index="index">
+            <slick-list v-model="moduleObj[type].children" :distance="30" lockAxis="y">
+              <slick-item v-for="(item, index) in moduleObj[type].children" :key="index" :index="index">
                 <div class="advertisement-msg" @click="getIndex(index)">
                   <upload
                     :data.sync="item.detail.image_url"
@@ -60,7 +60,7 @@
               <span class="close hand" @click="hideModal"></span>
             </div>
             <!--课程列表-->
-            <div v-if="modalType === 3006" class="goods-modal">
+            <div v-if="modalType === 3013" class="goods-modal">
               <div class="shade-tab">
                 <base-search
                   v-model="modalKeyword"
@@ -75,7 +75,6 @@
                 <div class="goods-item goods-header">
                   <div class="goods-text"></div>
                   <div class="goods-text">文章封面</div>
-                  <div class="goods-text">标题</div>
                 </div>
                 <div class="goods-list">
                   <div v-for="(item, index) in courseList" :key="index" class="goods-item">
@@ -84,10 +83,10 @@
                         <span class="after"></span>
                       </div>
                     </div>
-                    <div class="goods-text goods-msg">
-                      <img :src="item.cover_image_url" alt="" class="goods-img">
-                    </div>
-                    <div class="goods-text">{{item.title}}</div>
+<!--                    <div class="goods-text goods-msg">-->
+<!--                      <img :src="item.cover_image_url" alt="" class="goods-img">-->
+<!--                    </div>-->
+                    <div class="goods-text">{{item.name}}</div>
                   </div>
                 </div>
               </div>
@@ -136,10 +135,13 @@
         addImage: require('./pic-add_img@2x.png'),
         bannerList: [],
         type: 'banner',
-        modalType: 3006,
+        modalType: 3013,
         moduleList: [],
+        moduleObj: {
+          banner: {children: []}
+        },
         showModal: false,
-        modalTypeList: [],
+        modalTypeList: [{title: '选择课程', status: 3013}, {title: '小程序链接', status: 3005}],
         tabIndex: [],
         left: 0,
         courseList: [],
@@ -151,14 +153,12 @@
       }
     },
     beforeRouteEnter(to, from, next) {
-      API.Cms.moduleShow({data: {code: 'gift_index'}})
+      API.Cms.moduleShow({data: {code: 'market_index'}})
         .then((res) => {
           next((vx) => {
             vx.moduleList = res.data.children
             res.data.children.forEach((item, idx) => {
-              if (item.code === 'banner') {
-                vx.bannerList = item.children
-              }
+              vx.moduleObj[item.code] = item
             })
           })
         })
@@ -167,15 +167,53 @@
         })
     },
     methods: {
+      getModule() {
+        API.Cms.moduleShow({data: {code: 'market_index'}})
+          .then((res) => {
+            this.moduleList = res.data.children
+            res.data.children.forEach((item, idx) => {
+              this.moduleObj[item.code] = item
+            })
+          })
+      },
       getIndex() {},
       deleteGoodsMainPic() {},
       successImage() {},
+      _getCourse() {
+        API.Cms.getCourseList({data: {page: 1, keyword: '', status: ''}}).then((res) => {
+          this.courseList = res.data
+        })
+      },
       showModalBox() {
+        this._getCourse()
         this.showModal = true
       },
       showConfirm() {},
-      newCms() {},
-      saveCms() {},
+      newCms() {
+        switch (this.type) {
+        case 'banner':
+          if (this[this.dataName].length >= 5) {
+            this.$toast.show('最多添加5个轮播图')
+          }
+          break
+        }
+        this.moduleObj[this.type].children.push({detail: {object_id: '', url: '', title: '', image_id: '',}, style: ''})
+      },
+      saveCms() {
+        this.moduleObj[this.type].children.push({
+          "detail": {
+            "object_id": "",
+            "url": "/page/home",
+            "title": "",
+            "image_id": 1772,
+            "image_url": "https://exchange-platform-1254297111.picgz.myqcloud.com/dev/2019/09/25/1569397929504-168145.jpeg"
+          }, "style": 3005, "parent_id": 161, "sort": 0
+        })
+        API.Cms.saveModule({data: {data: [this.moduleObj[this.type]]}}).then((res) => {
+          this.$toast.show('保存成功')
+          this.getModule()
+        })
+      },
       justifyForm() {},
       hideModal() {},
       modalConfirm() {},
@@ -261,6 +299,52 @@
       .content-title
         text-indent: 0
 
+  .model
+    width: 52.08vw
+    height: 592px
+    .shade-header
+      display: flex
+      justify-content: space-between
+      height: 52px
+      box-sizing: border-box
+      padding: 23px 0 0 0
+      .shade-tab-type
+        height: 100%
+        display: flex
+        position: relative
+        .shade-tab-item
+          position: relative
+          line-height: 1
+          display: flex
+          transition: all 0.3s
+          color: $color-text-main
+          font-family: $font-family-regular
+          margin-right: 40px
+        .shade-tab-item-active
+          font-family: $font-family-medium
+        .line
+          transition: all 0.3s
+          left: 0
+          position: absolute
+          bottom: 0
+          height: 3px
+          width: 32px
+          background: $color-main
+          border-radius: 3px
+      .shade-title
+        color: $color-text-main
+        font-family: $font-family-medium
+        font-size: $font-size-16
+      .close
+        icon-image('icon-close')
+        width: 12px
+        height: @width
+        transition: all 0.3s
+  .shade-tab
+    height: 68px
+    align-items: center
+    box-sizing: border-box
+    display: flex
   .advertisement-msg
     height: 140px
     padding: 0 20px
@@ -372,4 +456,119 @@
     line-height: 1
     user-select: none
     transition: all 0.2s
+
+  .goods-content
+    border-radius: 4px
+    height: 405px
+    .goods-list
+      flex-wrap: wrap
+      display: flex
+    .goods-item
+      box-sizing: border-box
+      padding: 0 20px 0 20px
+      width: 100%
+      height: 60px
+      display: flex
+      align-items: center
+      position: relative
+      &:last-child
+        border-bottom-1px($color-line)
+      &:before
+        content: ""
+        pointer-events: none // 解决iphone上的点击无效Bug
+        display: block
+        position: absolute
+        left: 0
+        top: 0
+        transform-origin: 0 0
+        border-right: 1px solid #E9ECEE
+        border-left: 1px solid #E9ECEE
+        border-top: 1px solid #E9ECEE
+        box-sizing border-box
+        width: 200%
+        height: 100%
+        transform: scaleX(.5) translateZ(0)
+        @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3)
+          width: 100%
+          height: 300%
+          transform: scaleX(1 / 3) translateZ(0)
+      &:nth-child(2n)
+        background: #F4F8F9
+      .goods-img
+        margin-right: 10px
+        width: 40px
+        height: @width
+        min-height: @width
+        min-width: @width
+        object-fit: cover
+        overflow: hidden
+        background-repeat: no-repeat
+        background-size: cover
+        background-position: center
+        background-color: $color-background
+      .goods-name
+        no-wrap()
+      .goods-name, .goods-money
+        line-height: 1
+        font-size: $font-size-14
+      .add-btn
+        border-radius: 2px
+        margin-left: 88px
+        padding: 7px 0
+        width: 56px
+        text-align: center
+      .add-btn-disable
+        border-radius: 2px
+        margin-left: 88px
+        padding: 7px 0
+        width: 56px
+        box-sizing: border-box
+        text-align: center
+        font-size: $font-size-14
+        line-height: 1
+        cursor: not-allowed
+        background: $color-line
+        color: $color-text-assist
+    .goods-header
+      height: 45px
+      background: #F4F8F9
+      display: flex
+    .goods-text
+      font-size: $font-size-14
+      font-family: $font-family-regular
+      color: $color-text-main
+      &:nth-child(1)
+        min-width: 38px
+        max-width: 38px
+      &:nth-child(2)
+        flex: 2
+      &:nth-child(3)
+        flex: 1
+      &:nth-child(4)
+        max-width: 80px
+        flex: 0.5
+    .goods-msg
+      align-items: center
+      display: flex
+
+  //  单选框
+  .select-icon
+    width: 18px
+    height: 18px
+    border-radius: 50%
+    background: $color-white
+    border: 1px solid $color-line
+    transition: all, 0.3s
+    position: relative
+    transform-origin: 50%
+    margin-right: 20px
+    .after
+      all-center()
+      transform-origin: 50%
+      transition: all, 0.3s
+      width: 0
+      height: 0
+      border-radius: 50%
+  .select-icon-active
+    border: 5px solid $color-main
 </style>
