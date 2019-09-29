49bfdc4002a144f5e231369e146706eea4154e06
wengtianliang <wengtianliang@jkweixin.com>
2019-09-29 10:51:01 +0800

    router

--- vue.config.js
+++ vue.config.js
@@ -59,9 +59,9 @@ module.exports = {
   },
   css: {
     // 是否使用css分离插件 ExtractTextPlugin
-    extract: builded,
+    extract: false,
     // 开启 CSS source maps?
-    sourceMap: !builded,
+    sourceMap: false,
     // css预设器配置项
     loaderOptions: {},
     // 启用 CSS modules for all css / pre-processor files.
